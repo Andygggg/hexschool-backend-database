977a5dc3e3c9363763091dd729f224dd35573c41
Andygggg <andyhello31468@gmail.com>
2024-11-30 17:49:56 +0800

    fix: debug2

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -239,9 +239,10 @@ WHERE user_id = (select id from "USER" where email = 'wXlTq@hexschooltest.io')
 GROUP BY user_id;
 
 -- 5-7. 查詢：計算用戶王小明的已使用堂數，顯示須包含以下欄位： user_id , total。 (需使用到 Count 函式與 Group By)
-SELECT user_id, count(*) as total FROM "COURSE_BOOKING"
-WHERE user_id = (select id from "USER" where email = 'wXlTq@hexschooltest.io')
-and status != '課程已取消'
+SELECT user_id, COUNT(*) as total 
+FROM course_booking
+WHERE user_id = (SELECT id FROM "user" WHERE email = 'wXlTq@hexschooltest.io')
+AND status != '課程已取消'
 GROUP BY user_id;
 
 -- 5-8. [挑戰題] 查詢：請在一次查詢中，計算用戶王小明的剩餘可用堂數，顯示須包含以下欄位： user_id , remaining_credit
